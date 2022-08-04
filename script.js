1c2a5641239dce77d0c8ceeb408ed715a970e7fe
Chow <contact-chxw@gmail.com>
2022-08-04 16:43:53 -0400

    forms update, project QOL change to js

--- script.js
+++ script.js
@@ -87,7 +87,6 @@ portfolio.mobileMenu = () => {
 
     //closing the menu by clicking anywhere
     document.addEventListener('click', function(e) {
-        console.log(e.target)
         if (e.target.id !== 'hamburger' && e.target.id !== 'navLinks') {
             menuEl.classList.remove('showMenu');
             topbunEl.classList.remove('topBunClosed');
@@ -97,10 +96,63 @@ portfolio.mobileMenu = () => {
     })
 }
 
+portfolio.myProjects = [
+    {
+        asset: './images/urban-score.mp4',
+        title: 'Urban Score',
+        tools: 'HTML5 / Sass / Javascript / REST API / Pair Programming',
+        desc: `Urban City is an app that's powered by vanilla Javascript and REST API. It has all the world's urban cities. Based on your selected city, it will give a score based on that city's quality of life. It measures everything from housing to education. You can save as many cities as you wish to compare.`,
+        liveURL: 'https://urban-score.netlify.app/',
+        repoURL: 'https://github.com/Max-Chow-project2/urban-score'
+    },
+    {
+        asset: './images/urban-score.mp4',
+        title: 'GameLogo+',
+        tools: 'HTML5 / Sass / jQuery /Photoshop',
+        desc: `A fun quiz powered by jQuery that tests your knowledge of various video game studios' logos. Logos have been altered with Adobe Photoshop to be extra hard. Can you guess them all?`,
+        liveURL: 'https://gamelogoquiz.netlify.app/',
+        repoURL: 'https://github.com/chxwcodes/gameStudioQuiz'
+    },
+    {
+        asset: './images/urban-score.mp4',
+        title: `What's 4 Dinnr?`,
+        tools: 'HTML5 / Sass / jQuery /REST API',
+        desc: `Tired and can't decide on what to make for dinner? Want to get rid of whatever foods that's left over in your fridge before grocery shopping? Let this app decide for you based on what you have in your fridge! It also considers your calories and dietary restrictions.`,
+        liveURL: 'https://gamelogoquiz.netlify.app/',
+        repoURL: 'https://github.com/chxwcodes/gameStudioQuiz'
+    }
+]
+
+portfolio.populateProjects = () => {
+    const projectSectionEl = document.querySelector('#projects .wrapper');
+
+    portfolio.myProjects.forEach(project => {
+        const projectContainer = document.createElement('article');
+        projectContainer.classList.add('workItem');
+        projectContainer.innerHTML = `
+        <div class="projectImgContainer">
+            <video src=${project.asset} autoplay loop playsinline muted></video>
+        </div>
+        <div class="projectTextContainer">
+            <h3>${project.title}</h3>
+            <p class="projectTools">${project.tools}</p>
+            <p>${project.desc}</p>
+
+            <div class="projectLinks">
+                <a href=${project.liveURL} class="cyanButton">View live</a>
+                <a href=${project.repoURL} class="blackButtonOutline">View repo</a>
+            </div>
+        </div>
+        `;
+        projectSectionEl.appendChild(projectContainer);
+    })
+}
+
 portfolio.init = () => {
     portfolio.typewriterEffect(portfolio.wordArray);
     portfolio.navBarToggle();
     portfolio.mobileMenu();
+    portfolio.populateProjects();
 }
 
 portfolio.init();
